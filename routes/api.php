c12c25a56607b98443f863f2fb96fadfbd3d572a
yiwen <tyiwen27@gmail.com>
2022-08-22 12:40:28 +0800

    main - Add api routes

--- routes/api.php
+++ routes/api.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::group(['prefix' => 'point', 'as' => 'point.'], function (): void {
+    Route::post('/redeem', 'App\Http\Controllers\Point\RedeemPointController@execute')->name('redeem');
+    Route::post('/reward', 'App\Http\Controllers\Point\RewardPointController@execute')->name('reward');
+});
 
